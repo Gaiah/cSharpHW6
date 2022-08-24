e41a5fb91d4ea190f2f85c5256a4799c1a07ce3f
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-24 12:22:37 +0400

    Task_44 added

---
+++ Task_44/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter Fibonacci number: ");
+int FibonacciNum = int.Parse(Console.ReadLine()?? "0");
+int[] FiboArray = new int[FibonacciNum];
+Console.Write($"Here are first {FibonacciNum} elements of Fibonacci series: 0   1   ");
+
+for (int i = 2; i < FibonacciNum; i++)
+{
+    FiboArray[0] = 0;
+    FiboArray[1] = 1;
+    FiboArray[i] = FiboArray[i - 1] + FiboArray[i - 2];
+    Console.Write($"{FiboArray[i]}   ");
+}
