e8792920427965bb08ecf421e384b9418184191a
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-25 09:42:57 +0400

    Task_41 added

---
+++ Task_41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void ShowArrayAndCount (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"You've entered {count} positive number(s)");
+}
+
+Console.WriteLine("How many numbers do You want to input? ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] NumArray = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Input {i + 1} number: ");
+    NumArray[i] = int.Parse(Console.ReadLine() ?? "0");        
+}
+
+ShowArrayAndCount(NumArray);
+
